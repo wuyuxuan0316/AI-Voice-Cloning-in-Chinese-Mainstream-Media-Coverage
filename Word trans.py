f90197b82362ba376daf64b20eba945a26d5155c
wuyuxuan0316 <wuyuxuan0316@gmail.com>
2024-09-03 09:55:37 +0100

    Add files via upload

---
+++ Word trans.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Load the CSV file to check its contents
+file_path = '/Users/wuyuxuan/Documents/dissertation/Data/ThePaper/text/translated_PPS1.csv'
+data = pd.read_csv(file_path)
+data.head()
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+# Generate a word cloud image
+word_freq = dict(zip(data['english_word'], data['freq']))
+font_path = '/System/Library/Fonts/STHeiti Light.ttc'
+wordcloud = WordCloud(font_path=font_path, width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
+
+
+# Display the generated image
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.savefig('/Users/wuyuxuan/Documents/dissertation/Data/ThePaper/image/wordPPS1_e.png') 
+plt.show()
